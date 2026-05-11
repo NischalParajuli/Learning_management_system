40fa3c0ea8bff51a5aa3b923d508d1b1caf335b3
NischalParajuli <parajulinischal204@gmail.com>
2026-05-11 11:25:13 +0545

    Added Sponsor viewset

--- sponsorship/views.py
+++ sponsorship/views.py
@@ -1,3 +1,69 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from accounts.permissions import IsSponsor , IsAdmin
+from .models import Sponsorship , CourseSponsor
+from .serializer import SponsorshipSerializer , CourseSponsorSerializer
+from rest_framework.permissions import IsAuthenticated
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework.filters import SearchFilter
+
 
 # Create your views here.
+class SponsorshipViewset(viewsets.ModelViewSet):
+  queryset = Sponsorship.objects.all()
+  serializer_class = SponsorshipSerializer
+  filter_backends = [DjangoFilterBackend,SearchFilter]
+  filterset_fields = ['amount','sponsored_at','status']
+  search_fields = ['enrollment__course__title','enrollment__student__username','sponsor__username']
+
+  def get_queryset(self):
+    user = self.request.user
+
+    if user.role == 'admin':
+      return Sponsorship.objects.all()
+    
+    if user.role == 'sponsor':
+      return Sponsorship.objects.filter(sponsor = user)
+    
+    return Sponsorship.objects.none()
+
+
+
+  def get_permissions(self):
+    if self.action in ['create','update','partial_update','destroy']:
+      return[IsSponsor() | IsAdmin()]
+    return[IsAuthenticated()]
+  
+
+  def perform_create(self, serializer):
+    serializer.save(sponsor=self.request.user)
+
+
+class CourseSponsorViewset(viewsets.ModelViewSet):
+  queryset = CourseSponsor.objects.all()
+  serializer_class = CourseSponsorSerializer
+  filter_backends = [DjangoFilterBackend,SearchFilter]
+  filterset_fields = ['amount','sponsored_at','status']
+  search_fields = ['enrollment__course__title','sponsor__username']
+
+
+  def get_queryset(self):
+    user = self.request.user
+
+    if user.role == 'admin':
+      return CourseSponsor.objects.all()
+    
+    if user.role == 'sponsor':
+      return CourseSponsor.objects.filter(sponsor = user)
+    
+    return CourseSponsor.objects.none()
+  
+
+  def get_permissions(self):
+    if self.action in ['create','update','partial_update','destroy']:
+      return[IsSponsor() | IsAdmin()]
+    return[IsAuthenticated()]
+  
+  def perform_create(self, serializer):
+    serializer.save(sponsor=self.request.user)
+
